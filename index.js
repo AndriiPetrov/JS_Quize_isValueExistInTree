75ae3c95a1066c8694f265180b0ad8fb244d58e7
Andrii_Petrov2 <andrii_petrov2@epam.com>
2021-11-11 14:07:09 +0200

    Solution 2,3

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+// Solution #1 one line
 const isVavlueExistInTree = (arr, number) => arr.flat(Infinity).includes(number);
+
+// Solution #2 recursive flat
+const flatDeep = (arr, d = 1) => {
+  return d > 0
+    ? arr.reduce(
+        (acc, val) =>
+          acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val),
+        []
+      )
+    : arr.slice();
+}
+
+const isVavlueExistInTree = (arr, number) => flatDeep(arr, Infinity).includes(number);
+
+// Solution #3 // Not recursive flatten
+const flatten = (arr) => {
+  const stack = [...arr];
+  const res = [];
+  while (stack.length) {
+    const next = stack.pop();
+    if (Array.isArray(next)) {
+      stack.push(...next);
+    } else {
+      res.push(next);
+    }
+  }
+  return res.reverse();
+}
+
+const isVavlueExistInTree = (arr, number) => flatten(arr).includes(number);
